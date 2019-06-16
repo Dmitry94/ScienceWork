7717b0c83d4ccf9d19019e4956369c5472b892a1
Ermolaev Dmitry <dimonermolaev1994@gmail.com>
2019-06-16 18:48:13 +0300

    Draw of bab solver.

--- pbab/pbab_babsolver.hpp
+++ pbab/pbab_babsolver.hpp
@@ -1,4 +1,30 @@
 #ifndef PBAB_BABSOLVER_HPP
 #define PBAB_BABSOLVER_HPP
 
+#include "pbab_solver.hpp"
+
+namespace pbab {
+
+/**
+ * Abstract class of solver which is based on branch and bound method.
+ */
+class BABSolver : public Solver {
+protected:
+    /**
+     * Get the simpliest solution.
+     * @return some solution, which may be usefull in the start.
+     */
+    virtual T get_trivial_answer() const = 0;
+
+    /**
+     * Sometimes in process of searching best solution
+     * it is necessary to recalculate solution analyzers.
+     * In derrived classes it can be empty.
+     */
+    virtual void recalc_solanalyzers() = 0;
+
+};
+
+}
+
 #endif // PBAB_BABSOLVER_HPP
